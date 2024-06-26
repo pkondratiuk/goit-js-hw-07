665edb2c150c85cd1e8b36ce635140dda3953578
Pavlo <95445682+pkondratiuk@users.noreply.github.com>
2024-06-26 23:53:23 +0300

    update task-4
    add Event listner and deafult parametr for submit

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,9 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handlerSubmit);
+function handlerSubmit(event) {
+    event.preventDefault();
+
+    const { email, password } = event.currentTarget.elements;
+    
+}
