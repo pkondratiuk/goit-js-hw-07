55906726af6d1dfddbae583190a43f19ac147aa9
Pavlo <95445682+pkondratiuk@users.noreply.github.com>
2024-06-28 19:53:57 +0300

    task-4 and 5 finish

--- js/task-4.js
+++ js/task-4.js
@@ -5,5 +5,12 @@ function handlerSubmit(event) {
     event.preventDefault();
 
     const { email, password } = event.currentTarget.elements;
+    if (email.value === '' || password.value === '') {
+        return alert('Please fill in all the fields!');
+    }
+
+    const userData = { email: email.value, password: password.value };
+    console.log(userData);
+    event.currentTarget.reset();
     
 }
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColor = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const colorValue = document.querySelector('.color');
+
+changeColor.addEventListener('click', event => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorValue.textContent = color;
+
+})
+
