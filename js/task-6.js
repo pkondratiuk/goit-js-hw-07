94ed8ea94828b470b38813cec8e06ea070a5125f
Pavlo <95445682+pkondratiuk@users.noreply.github.com>
2024-07-01 23:02:43 +0300

    upd- task-6-hw-ja07
    finish task-6, need to change task-2 and task5

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,44 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const form = document.querySelector('#controls');
+const input = form.firstElementChild;
+const createButton = input.nextElementSibling;
+const destroyButton = form.lastElementChild;
+const boxes = document.querySelector('#boxes');
+
+
+function createBoxes(amount) {
+  const someDiv = [];
+  for (let i = 0; i < amount; i++) {
+    const element = document.createElement('div');
+    const size = 30 + i * 10;
+    element.style.width = `${size}px`;
+    element.style.height = `${size}px`;
+    element.style.backgroundColor = getRandomHexColor();
+    someDiv.push(element);
+    
+  }
+  boxes.append(...someDiv);
+  
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
+createButton.addEventListener('click', handlerClick);
+
+function handlerClick() {
+  const amount = input.value;
+  if (amount >= 1 && amount <= 100) {
+    destroyBoxes();
+    createBoxes(amount);
+    input.value = '';
+  }
+}
+
+destroyButton.addEventListener('click', () => {
+  destroyBoxes();
+});
